0287c512e6eb24c686dcc0ead3ed338850eb2f4a
Nilesh Borase <158148138+borasenilesh828@users.noreply.github.com>
2025-03-13 11:03:08 +0530

    Delete Routes directory

--- Routes/visit.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const visitController = require('../Controllers/visit')
-
-router.post('/request-visit',visitController.requestVisit)
-
-module.exports = router
